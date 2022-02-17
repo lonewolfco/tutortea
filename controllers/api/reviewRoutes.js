657f588e0b4646b787c66ecef56fd1947fe164ec
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-02-17 00:45:14 -0800

    Tied together functionality for creating a review

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -10,7 +10,20 @@ const {
 
 // POST /api/reviews -- create one review
 router.post('/', withAuthJson, async (req, res) => {
-    const review = await Review.create(req.body);
+    try {
+        const reviewData = await Review.create({
+            ...req.body,
+            "user_id": req.session.user_id
+        });
+
+        const review = reviewData.toJSON();
+
+        res.json(review);
+    } catch (err) {
+        res.status(400).json({
+            message: "Bad request"
+        });
+    }
 });
 
 // PUT /api/reviews/:id -- update one review
