d34239fef7e16b3a9b2ed523e70081add175eafd
Lo Selby <loselby@icloud.com>
2022-02-16 18:59:29 -0800

    Update login.js
    updated path from users to user

--- public/js/login.js
+++ public/js/login.js
@@ -26,7 +26,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/user", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
