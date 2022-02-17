32624d7dcf5248f5981ceafe96c6f0f105866b93
Mason Walker <masonwalker30@gmail.com>
2022-02-16 19:54:17 -0800

    new Review File

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,6 +2,7 @@ const sequelize = require("../config/connection");
 const { User, Tutor } = require("../models");
 //needs additional code, await the tutor, review, users seeds files under line 4.
 const userData = require("./userSeeds.json");
+const reviewData = require("./reviewSeeds.json");
 const tutorData = require("./tutorSeeds.json");
 
 const seedAll = async () => {
