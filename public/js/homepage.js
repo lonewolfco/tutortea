d2d3825c068d3c06d3f1211b5551fe2d5104ba39
SamusMoran <kimberlym4488@comcast.net>
2022-02-26 09:25:14 -0800

    Adds rolling quotes to dashboard cards

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,21 +1,56 @@
-let quoteBlock = document.querySelector(".quoteBlock");
-let quotes = [
-  "Love and scandal are the best sweeteners of tea. ~Henry Fielding~",
-  "Hello, is it tea, you're looking for....? ~Lionel 'T' Richie~",
-  "A simple cup of tea is far from a simple matter. ~Mary Lou Heiss~",
-  "I picked up my friend's coffee by accident then put it back. It wasn't my cup of tea. ~Anonymous~",
-  "We are like tea, we do not know our own strength until we are in Hot Water. ~Sister Busche~",
-  "Varie-tea is the spice of life! ~Collabrewators~",
-  "Some people will tell you there is a great deal of poetry and fine sentiment in a chest of tea. ~Ralph Waldo Emerson~",
-  "Hones-tea is the best policy. ~Anonymous~",
-];
+const dashFacts = document.querySelector(".dashFacts");
+const dashFacts1 = document.querySelector(".dashFacts1");
+const dashFacts2 = document.querySelector(".dashFacts2");
 var currentIndex = -1;
-function advanceTeaFacts() {
+const quotes = [
+  "The drink of winners.",
+  "Let’s get this par-tea started!",
+  "Wisdom in a cup.",
+];
+var currentIndex1 = -1;
+const quotes1 = [
+  "Filled to the brim.",
+  "Love at first sip.",
+  "Fancy a cup of tea?",
+];
+var currentIndex2 = -1;
+const quotes2 = [
+  "Every cup tells a story.",
+  "Don’t be chai!",
+  "Time to get chat-tea",
+  "We'll be-leaf you",
+];
+
+function startFacts() {
+  dashFactsStart();
+  dashFactsStart1();
+  dashFactsStart2();
+}
+// scrolling quotes on View Tutors
+function dashFactsStart() {
   ++currentIndex;
   if (currentIndex >= quotes.length) {
     currentIndex = 0;
   }
-  quoteBlock.innerHTML = quotes[currentIndex];
+  dashFacts.innerHTML = quotes[currentIndex];
 }
 
-var teaFacts = setInterval(advanceTeaFacts, 8000);
+// scrolling quote on See Reviews
+function dashFactsStart1() {
+  ++currentIndex1;
+  if (currentIndex1 >= quotes1.length) {
+    currentIndex1 = 0;
+  }
+  dashFacts1.innerHTML = quotes1[currentIndex1];
+}
+
+// scrolling quotes on View Tutors
+function dashFactsStart2() {
+  ++currentIndex2;
+  if (currentIndex2 >= quotes2.length) {
+    currentIndex2 = 0;
+  }
+  dashFacts2.innerHTML = quotes2[currentIndex2];
+}
+
+setInterval(startFacts, 5000);
