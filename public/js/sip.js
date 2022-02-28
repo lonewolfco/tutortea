c5d0c0f84cc905d75a232850ddd931db56707e02
SamusMoran <kimberlym4488@comcast.net>
2022-02-28 13:05:41 -0800

    Sort by functionality added to reviews

---
+++ public/js/sip.js
@@ -0,0 +1,23 @@
+//https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+//sorting functions for reviews
+
+// query selectors and event listeners for table head. Start with second row of table
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work...
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+  const table = th.closest('table');
+  const tbody = table.querySelector('tbody');
+  Array.from(tbody.querySelectorAll('tr'))
+    .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+    .forEach(tr => tbody.appendChild(tr) );
+})));
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
